416c23cd35898bd385b505de813431062c870b11
hatuyuki3569 <hirahirakaito3569@gmail.com>
2022-01-28 11:34:34 +0900

    いいね！機能を非同期で実装

--- Views/common/tweet.php
+++ Views/common/tweet.php
@@ -1,24 +1,24 @@
 <div class="tweet">
     <div class="user">
-        <a href="profile.php?user_id=<?php echo $view_tweet['user_id']?>">
-            <img src="<?php  echo buildImagePath($view_tweet['user_image_name'],'user'); ?>" alt="">
+        <a href="profile.php?user_id=<?php echo htmlspecialchars($view_tweet['user_id']); ?>">
+            <img src="<?php echo buildImagePath($view_tweet['user_image_name'], 'user'); ?>" alt="">
         </a>
     </div>
     <div class="content">
         <div class="name">
             <a href="profile.php?user_id=<?php echo htmlspecialchars($view_tweet['user_id']);?>">
-                <span class="nickname"><?php echo htmlspecialchars($view_tweet['user_nickname']);?></span>
-                <span class="user-name"><?php echo htmlspecialchars($view_tweet['user_name']);?>・<?php echo convertToDayTimeAgo($view_tweet['tweet_created_at'])?></span>
+                <span class="nickname"><?php echo htmlspecialchars($view_tweet['user_nickname']); ?></span>
+                <span class="user-name"><?php echo htmlspecialchars($view_tweet['user_name']);?>・<?php echo convertToDayTimeAgo($view_tweet['tweet_created_at']); ?></span>
             </a>
         </div>
-        <p><?php echo $view_tweet['tweet_body']?></p>
-        <?php if(isset($view_tweet['tweet_image_name'])) :?>
+        <p><?php echo $view_tweet['tweet_body'] ?></p>
+        <?php if (isset($view_tweet['tweet_image_name'])) : ?>
             <img src="<?php echo buildImagePath($view_tweet['tweet_image_name'],'tweet'); ?>" alt="" class="post-image">
-        <?php endif;?>
+        <?php endif; ?>
         <div class="icon-list">
-            <div class="like js-like" data-like-id="<?php echo htmlspecialchars($view_tweet['like_id']); ?>">
+            <div class="like js-like" data-tweet-id="<?php echo htmlspecialchars($view_tweet['tweet_id']); ?>" data-like-id="<?php echo htmlspecialchars($view_tweet['like_id']); ?>">
                 <?php
-                if(isset($view_tweet['like_id'])) {
+                if (isset($view_tweet['like_id'])) {
                     //いいねされている場合青のハートを表示
                     echo '<img src="' . HOME_URL . 'Views/img/icon-heart-twitterblue.svg" alt="">';
                 } else {
--- Views/js/likes.js
+++ Views/js/likes.js
@@ -3,15 +3,27 @@
 //////////////////////////////
 
 $(function(){
-        //いいね！がクリックされたとき
-        $('.js-like').click(function(){
-            const this_obj = $(this);
-            const like_id = $(this).data('like-id');
-            const like_count_obj = $(this).parent().find('.js-like-count');
-            let like_count = Number(like_count_obj.html());
+    //いいね！がクリックされたとき
+    $('.js-like').click(function(){
+        const this_obj = $(this);
+        const tweet_id = $(this).data('tweet-id');
+        const like_id = $(this).data('like-id');
+        const like_count_obj = $(this).parent().find('.js-like-count');
+        let like_count = Number(like_count_obj.html());
 
-            if(like_id){
-                //いいね！取り消し
+        if(like_id){
+            //いいね！取り消し
+            //非同期通信
+            $.ajax({
+                url: 'like.php',
+                type: 'POST',
+                data: {
+                    'like_id': like_id
+                },
+                timeout: 10000
+            })
+            // 取り消しが成功
+            .done(() => {
                 //!カウントを減らす
                 like_count--;
                 like_count_obj.html(like_count);
@@ -19,16 +31,35 @@ $(function(){
                 
                 //いいね！ボタンの色をグレーに変更
                 $(this).find('img').attr('src', '../Views/img/icon-heart.svg');
-            }else{
-                //いいね！付与
-                //!カウントを増やす
-                like_count++;
-                like_count_obj.html(like_count);
-                this_obj.data('like-id', true);
-                
-                //いいね！ボタンの色を青に変更
-                
-                $(this).find('img').attr('src', '../Views/img/icon-heart-twitterblue.svg');
-            }
-        });
+            })
+            .fail((data) => {
+                alert('処理中にエラーが発生しました。');
+                console.log(data);
+            });
+        }else{
+            //いいね！付与
+            //非同期通信
+            $.ajax({
+                url: 'like.php',
+                type: 'POST',
+                data: {
+                    'tweet_id': tweet_id
+                },
+                timeout: 10000
+            })
+            //いいね！が成功
+            .done((data) => {
+            //!カウントを増やす
+            like_count++;
+            like_count_obj.html(like_count);
+            this_obj.data('like-id', data['like_id']);
+            //いいね！ボタンの色を青に変更
+            $(this).find('img').attr('src', '../Views/img/icon-heart-twitterblue.svg');
+            })
+            .fail((data) => {
+                alert('処理中にエラーが発生しました。');
+                console.log(data);
+            });
+        }
+    });
 })
